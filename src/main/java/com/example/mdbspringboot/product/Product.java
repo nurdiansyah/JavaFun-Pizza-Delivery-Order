a300581d2c7b1dd3edac007b86b02f0f077c56ad
RadBile2022 <wahradar511@gmail.com>
2023-11-20 13:07:11 +0700

    push jar

--- src/main/java/com/example/mdbspringboot/product/Product.java
+++ src/main/java/com/example/mdbspringboot/product/Product.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "products")
@@ -23,7 +24,7 @@ public class Product {
     private String iconPrep;
     private String iconCook;
     private double iconPrice;
-    private   Bonus[] bonusBundling ;
+    private   List<Bonus> bonusBundling = new ArrayList<>();
 
 
 }
--- src/test/java/com/example/mdbspringboot/MdbSpringBootApplicationTests.java
+++ src/test/java/com/example/mdbspringboot/MdbSpringBootApplicationTests.java
@@ -28,13 +28,13 @@ class MdbSpringBootApplicationTests {
 	}
 	@Test
 	public void testProduct(){
-		List<Bonus> bonusList [] = new List[2];
+		List<Bonus> bonusList  = new ArrayList<>();
 		Bonus bonus1 = new Bonus("Es Jeruk","/image/esjeruk");
 		Bonus bonus2 = new Bonus("Appertize","/image/appertize");
-		bonusList[0]= (List<Bonus>) bonus1;
-		bonusList[1]= (List<Bonus>) bonus2;
+		bonusList.add(bonus1);
+		bonusList.add(bonus2) ;
 
-		Product product = new Product("123","/image/frankfuther","Frank Further","Pizza","Toping Unta","10 Menit", "10 Menit", 435345.0, (Bonus[]) bonusList);
+		Product product = new Product("123","/image/frankfuther","Frank Further","Pizza","Toping Unta","10 Menit", "10 Menit", 435345.0, bonusList);
 		assertEquals("same", product.getId(),"123");
 		assertEquals("same", product.getName(),"Frank Further");
 
